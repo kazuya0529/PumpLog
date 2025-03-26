25a8055439748433bf10f19dc97710f708e57776
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-27 01:46:55 +0900

    入力内容をテーブルに保存できるように記述

--- app/controllers/workout_menus_controller.rb
+++ app/controllers/workout_menus_controller.rb
@@ -3,8 +3,27 @@ class WorkoutMenusController < ApplicationController
   end
 
   def new
+    @workout_menu = WorkoutMenu.new
   end
 
   def create
+    @workout_menu = WorkoutMenu.new(workout_menu_params)
+    if @workout_menu.save
+      redirect_to workout_menus_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def workout_menu_params
+    params.require(:workout_menu).permit(
+      :day_of_week_id,
+      :menu_id,
+      :rep_id,
+      :set_id,
+      :dumbbell_weight_id
+    )
   end
 end
