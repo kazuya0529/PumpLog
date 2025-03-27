027b903f54c392ea66d22f2210b431c3388b1813
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-27 12:24:22 +0900

    dumbbell weightのプルダウンを5kg単位から1kg単位で選択できるように変更

--- app/models/dumbbell_weight.rb
+++ app/models/dumbbell_weight.rb
@@ -1,7 +1,7 @@
 class DumbbellWeight < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' }
-  ] + (5..50).step(5).map.with_index(1) { |kg, i| { id: i + 1, name: "#{kg}kg" } }
+  ] + (5..50).map.with_index(2) { |kg, i| { id: i, name: "#{kg}kg" } }
 
   include ActiveHash::Associations
   has_many :workout_menus
