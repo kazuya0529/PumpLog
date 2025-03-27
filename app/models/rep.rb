755acc1e66836e169e0fb67ef0f6f6ac73ed5259
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-27 12:31:03 +0900

    プルダウンで語尾にrepが表示されるように変更

--- app/models/rep.rb
+++ app/models/rep.rb
@@ -1,7 +1,7 @@
 class Rep < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' }
-  ] + (1..30).map { |n| { id: n + 1, name: n.to_s } }
+  ] + (1..30).map.with_index(2) { |num, i| { id: i, name: "#{num}rep" } }
 
   include ActiveHash::Associations
   has_many :workout_menus
