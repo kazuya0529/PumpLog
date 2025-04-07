fa8107e47ac99b22e751f890a4e77674a680d106
kazuya0529 <sabotenchocobo@gmail.com>
2025-04-07 16:15:24 +0900

    未ログイン状態のユーザーを転送するように記述

--- app/controllers/workout_menus_controller.rb
+++ app/controllers/workout_menus_controller.rb
@@ -1,4 +1,5 @@
 class WorkoutMenusController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
     @workout_menus = WorkoutMenu.all
   end
@@ -26,5 +27,11 @@ class WorkoutMenusController < ApplicationController
       :dumbbell_weight_id,
       :date
     )
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
