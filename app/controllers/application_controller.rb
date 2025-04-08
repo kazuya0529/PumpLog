e81abc6c70823884652f05c99d2d2326dbf98efe
kazuya0529 <sabotenchocobo@gmail.com>
2025-04-08 15:57:09 +0900

    トップページの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    workout_menus_path
+  end
+
+  def after_sign_up_path_for(resource)
+    workout_menus_path
+  end
+
   private
 
   def basic_auth
